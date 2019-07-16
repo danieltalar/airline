a58d3c1b7eddf75f9dc3d84b8c28302601c083ab
kakapela <kacperkapela1@gmail.com>
2019-07-16 11:14:40 +0200

    Change CrudRepository to Neo4jRepo

--- src/main/java/com/inz/airline/repository/CityRepository.java
+++ src/main/java/com/inz/airline/repository/CityRepository.java
@@ -1,11 +1,12 @@
 package com.inz.airline.repository;
 
 import com.inz.airline.domain.City;
+import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CityRepository extends CrudRepository<City, Long> {
+public interface CityRepository extends Neo4jRepository<City, Long> {
 
     City getByName(String name);
 }
--- src/main/java/com/inz/airline/repository/FlightRepository.java
+++ src/main/java/com/inz/airline/repository/FlightRepository.java
@@ -1,11 +1,13 @@
 package com.inz.airline.repository;
 
 import com.inz.airline.domain.Flight;
+import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FlightRepository extends CrudRepository<Flight, String> {
+public interface FlightRepository extends Neo4jRepository<Flight, String> {
 
     Flight getByCode(String code);
+
 }
