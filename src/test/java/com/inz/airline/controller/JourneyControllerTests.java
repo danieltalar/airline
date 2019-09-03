b6b5b9ead402686e4b90a85efa1cba316174c6e4
kakapela <kacperkapela1@gmail.com>
2019-09-03 10:32:34 +0200

    Modifying JourneyControllerTests

--- src/test/java/com/inz/airline/controller/JourneyControllerTests.java
+++ src/test/java/com/inz/airline/controller/JourneyControllerTests.java
@@ -1,21 +1,28 @@
 package com.inz.airline.controller;
 
+import com.inz.airline.service.JourneyService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+
 
 @RunWith(SpringRunner.class)
 @WebAppConfiguration
-@WebMvcTest(JourneyController.class)
+@AutoConfigureMockMvc
+@SpringBootTest
 public class JourneyControllerTests {
 
-//    @Autowired
-//    private MockMvc mvc;
+    @Autowired
+    private MockMvc mvc;
 
-//    @MockBean
-//    private JourneyService service;
+    @MockBean
+    private JourneyService service;
 
     @Test
     public void givenEmployees_whenGetEmployees_thenReturnJsonArray()
