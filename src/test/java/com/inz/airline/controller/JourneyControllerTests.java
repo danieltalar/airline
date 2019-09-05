31f9a8910c4e2cf618543729779ba68aedeb8adb
danieltalar <daniel.talar@wp.pl>
2019-09-05 10:12:26 +0200

    Testy API

--- src/test/java/com/inz/airline/controller/JourneyControllerTests.java
+++ src/test/java/com/inz/airline/controller/JourneyControllerTests.java
@@ -1,49 +1,57 @@
 package com.inz.airline.controller;
 
-import com.inz.airline.service.JourneyService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import com.inz.airline.dto.SearchFlightDto;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @RunWith(SpringRunner.class)
 @WebAppConfiguration
-@AutoConfigureMockMvc
+@AutoConfigureMockMvc(secure = false, addFilters = false)
 @SpringBootTest
 public class JourneyControllerTests {
+
 
     @Autowired
     private MockMvc mvc;
 
-    @MockBean
-    private JourneyService service;
 
     @Test
     public void givenEmployees_whenGetEmployees_thenReturnJsonArray()
             throws Exception {
-//        SearchFlightDto searchFlightDto = new SearchFlightDto();
-//        searchFlightDto.setCityTo("London");
-//        searchFlightDto.setCityTo("Barcelona");
-//
-//        //Employee alex = new Employee("alex");
-//
-//        List<Journey> allEmployees = new ArrayList<>();
-//
-//
-//        given(service.getJourney(searchFlightDto)).willReturn(allEmployees);
-//
-//        mvc.perform(get("/api/journey")
-//                .contentType(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$", hasSize(1)));
-//                .andExpect(jsonPath("$[0].name", is(alex.getName())));
-    }
+        SearchFlightDto searchFlightDto = new SearchFlightDto();
+        searchFlightDto.setCityTo("Istanbul");
+        searchFlightDto.setCityFrom("New York");
+        searchFlightDto.setCountAdult(1);
+        searchFlightDto.setCountChildren(3);
+        searchFlightDto.setTicketType("economy");
+        searchFlightDto.setDataStartSearch(LocalDateTime.now());
 
-    // write test cases here
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
+        ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
+        String requestJson = ow.writeValueAsString(searchFlightDto);
+
+        mvc.perform(post("/api/journey").contentType(APPLICATION_JSON_UTF8)
+                .content(requestJson))
+                .andExpect(status().isOk());
+
+    }
 }
